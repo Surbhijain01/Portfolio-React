f61d8306722b58533498ca79f74f07e59745ed00
Surbhi Jain <136691725+Surbhijain01@users.noreply.github.com>
2024-01-07 19:41:38 +0530

    Add files via upload

---
+++ public/projectInfo.js
@@ -0,0 +1,34 @@
+let projectArray =[ {
+    imageLink: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPDGJqXOUxmJSkX1G15DKYtM0POHiKH2qNLBGdpgpU3N12eP9cQZhGiW3uhd-hlGvsbuQ&usqp=CAU",
+    projectTitle: "Tic Tac Toe",
+    projectDes: "Designed and coded an immersive Tic-Tac-Toe game with HTML, CSS, and JavaScript.",
+    github: "https://github.com/Surbhijain01/TicTacToe",
+    projectLink: "https://surbhijain01.github.io/TicTacToe/",
+  },
+   {
+
+    imageLink: "../src/images/ToDoList.jpeg",
+    projectTitle: "To-Do-List",
+    projectDes: "I created a dynamic to-do list using HTML, CSS, and JavaScript. Simplify task management with an intuitive interface.",
+    github: "https://github.com/Surbhijain01/ToDoList",
+    projectLink: "https://surbhijain01.github.io/ToDoList/",
+
+  },
+
+  {
+    imageLink : "../src/images/project 1.0.png",
+        projectTitle : "Netflix Clone",
+        projectDes : "Portfolio includes a Netflix clone crafted with HTML and CSS, showcasing my frontend skills in recreating a modern and responsive user interface.",
+        github : "https://github.com/Surbhijain01/Netflix-Clone",
+        projectLink : "https://surbhijain01.github.io/Netflix-Clone/",
+  },
+  {
+      
+       imageLink : "../src/images/project 1.2.png",
+        projectTitle : "RealTimeNews Website",
+        projectDes : "News website using HTML, CSS, JavaScript, and Bootstrap for a responsive design. Integrated real-time news updates through APIs, providing users with the latest information.",
+        github : "https://github.com/Surbhijain01/Real-Time-News-Website",
+        projectLink : "https://surbhijain01.github.io/Real-Time-News-Website/",
+  }]
+
+  export default projectArray;
